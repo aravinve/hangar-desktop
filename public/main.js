222df3ac62e30032630dfeef7c344388aff80d6b
Aravind Venkatesan <aravindtherocker@gmail.com>
2020-05-22 12:14:33 +0800

    clean up commit

--- public/main.js
+++ public/main.js
@@ -8,8 +8,6 @@ const url = require('url');
 const isDev = require('electron-is-dev');
 
 let mainWindow;
-let imageWindow;
-let settingsWindow;
 
 function createWindow() {
   mainWindow = new BrowserWindow({
@@ -19,48 +17,14 @@ function createWindow() {
       nodeIntegration: true,
     },
   });
-  imageWindow = new BrowserWindow({
-    width: 600,
-    height: 600,
-    parent: mainWindow,
-    show: false,
-    webPreferences: {
-      nodeIntegration: true,
-    },
-  });
-  settingsWindow = new BrowserWindow({
-    width: 600,
-    height: 600,
-    parent: mainWindow,
-    show: false,
-    webPreferences: {
-      nodeIntegration: true,
-    },
-  });
+
   mainWindow.loadURL(
     isDev
       ? 'http://localhost:3000'
       : `file://${path.join(__dirname, '../build/index.html')}`
   );
-  imageWindow.loadURL(
-    isDev
-      ? 'http://localhost:3000/image'
-      : `file://${path.join(__dirname, '../build/index.html')}`
-  );
-  settingsWindow.loadURL(
-    isDev
-      ? 'http://localhost:3000/settings'
-      : `file://${path.join(__dirname, '../build/index.html')}`
-  );
+
   mainWindow.on('closed', () => (mainWindow = null));
-  imageWindow.on('close', (e) => {
-    e.preventDefault();
-    imageWindow.hide();
-  });
-  settingsWindow.on('close', (e) => {
-    e.preventDefault();
-    settingsWindow.hide();
-  });
 }
 
 app.on('ready', createWindow);
@@ -75,13 +39,4 @@ app.on('activate', () => {
   if (mainWindow === null) {
     createWindow();
   }
-});
-
-ipcMain.on('toggle-image', (event, arg) => {
-  imageWindow.show();
-  imageWindow.webContents.send('image', arg);
-});
-
-ipcMain.on('toggle-settings', (event, arg) => {
-  settingsWindow.isVisible() ? settingsWindow.hide() : settingsWindow.show();
 });
--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Home from './Home';
-import Settings from './Settings';
-import Image from './Image';
+import Home from './components/home/Home';
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path='/' component={Home} />
-        <Route exact path='/image' component={Image} />
-        <Route exact path='/settings' component={Settings} />
       </Switch>
     </BrowserRouter>
   );
