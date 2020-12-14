7ec872c878e8d2e65d831de777abbb6d6cd48003
Aravind Venkatesan <aravindtherocker@gmail.com>
2020-12-14 19:12:52 +0800

    Maps Commit

---
+++ src/components/maps/Map.js
@@ -0,0 +1,36 @@
+import {useState} from 'react'
+import GoogleMapReact from 'google-map-react'
+import LocationMarker from './LocationMarker'
+import LocationInfoBox from './LocationInfoBox'
+
+function Map({eventData, center, zoom}) {
+    const [locationInfo, setLocationInfo] = useState(null)
+    const markers = eventData !== undefined ?
+         eventData.map(ev => {
+            if(ev.categories[0].id === 8){
+               return  <LocationMarker lat={ev.geometries[0].coordinates[1]} lng={ev.geometries[0].coordinates[0]} onClick={() => setLocationInfo({id: ev.id, title: ev.title})} />
+            }
+        })
+    : null
+    return (
+      <div className="w-screen h-screen relative">
+           <GoogleMapReact
+           bootstrapURLKeys={{key: process.env.REACT_APP_GOOGLE_MAP_API_KEY}}
+           defaultCenter={center}
+           defaultZoom={zoom}>
+               {markers}
+       </GoogleMapReact>
+       {locationInfo && <LocationInfoBox info={locationInfo} />}
+      </div>
+    )
+}
+
+Map.defaultProps = {
+    center: {
+        lat: 42.3265,
+        lng: -122.8756
+    },
+    zoom: 6
+}
+
+export default Map
---
+++ src/components/maps/SidePane.js
@@ -0,0 +1,40 @@
+function SidePane({loadSelectData}) {
+  const paneStyle = {
+    top: '2rem',
+    left: '2rem',
+    zIndex: '10'
+  }
+  return (
+    <div
+      className='absolute w-auto h-auto shadow-md rounded-md bg-secondary'
+      style={paneStyle}>
+      <nav>
+        <p className='bg-primary w-full p-2 rounded-t-md inline-flex items-center text-secondary text-xl select-none'>
+        <i className="fas fa-map-marker-alt mr-2"></i> Maps</p>
+        <div className='flex flex-row p-4'>
+          <div className='flex-1 inline-flex'>
+            <p className='text-primary text-sm inline-flex items-center mr-2'>
+            <i className='fas fa-filter mr-2'></i>
+              Filter</p>
+          </div>
+          <div className='inline-flex items-center m-1'>
+            <select
+              name='mapOptionName'
+              id='map-option-name'
+              className="rounded-md shadow-md p-1 text-sm text-primary outline-none focus:outline-none"
+              onChange={loadSelectData}
+            >
+              <option value='' selected disabled hidden>
+                Choose
+              </option>
+              <option value='normal'>Explore</option>
+              <option value='eo'>Earth Observatory</option>
+            </select>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default SidePane;
